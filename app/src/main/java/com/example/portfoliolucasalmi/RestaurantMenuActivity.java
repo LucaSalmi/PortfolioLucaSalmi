b1353a6bbfdb85d071d17c6ce1f4ea2ebbba3b09
LucaSalmi <luca.salmi90@gmail.com>
2021-11-09 13:19:52 +0100

    created RestaurantMenuActivity

---
+++ app/src/main/java/com/example/portfoliolucasalmi/RestaurantMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.portfoliolucasalmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_menu);
+    }
+}
